f6b36878f40d83c157729f5b4c0b5d5a53e3bb06
Emma Jaud <jaud2@att.net>
2020-12-22 01:36:22 -0600

    update angels pages

---
+++ assets/js/dell.js
@@ -0,0 +1,29 @@
+
+
+// ======= ANGELS JS ======= // 
+
+$(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='checkout']").validate({
+      // Specify validation rules
+      rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+        cardNumber: {
+          required: true,
+          creditcard: true
+        }
+      },
+      // Specify validation error messages
+      messages: {
+        cardNumber: "The credit card number you entered apprears to be invalid!"
+      },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
