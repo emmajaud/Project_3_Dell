ee8b651ed8996961e759a5000417d163092fb0d7
Emma Jaud <jaud2@att.net>
2021-01-08 00:07:41 -0600

    adding functionality to compare button

---
+++ assets/js/check.js
@@ -0,0 +1,21 @@
+$('#compareCheck').click(function(){
+    var compareColor = $(this).css("background-color");
+    console.log(compareColor);
+    $(this).css({"background-color": "#5f45bf", "color": "white", "border-color": "5f45bf"});
+
+});
+
+
+/*.add-wishlist{
+    background-color:#fff;
+    color:#256EFF;
+    font-family:'Roboto', sans-serif;
+    border-color:#256EFF;
+    border:1px;
+    border-style:solid;
+    border-radius: 8px;
+    justify-content: center;
+    display:flex;
+    padding:10px;
+    margin-top: 12px;
+  }rgb(2, 8, 135)*/
